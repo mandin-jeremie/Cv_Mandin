f6828438aa8493ca8bf5990502fbf8afec4386e3
Ubuntu-WSL <mandin.j@gmail.com>
2020-08-17 15:09:22 +0200

    Transition on click for the navigation and header set

--- src/container/Header/index.js
+++ src/container/Header/index.js
@@ -1,29 +1,35 @@
 import React from "react";
 
+import { Link } from 'react-scroll'
+
 import "./header.css";
 
-const Header = () => {
+
+const Header = ({ handleSetActive}) => {
+
   return (
-    <div className="bloc">
+    <div className="bloc_header_page">
+    <div className="bloc_header">
       <h1 className="title_name">J.M</h1>
-      <a className="navPage" href="#home">
+      <Link className="navPage" activeClass="active" to="home" spy={true} smooth={true} offset={0} duration={700} onSetActive={handleSetActive}>
         Acceuil
-      </a>
-      <a className="navPage" href="#skill">
+      </Link>
+      <Link className="navPage" activeClass="active" to="skill" spy={true} smooth={true} offset={0} duration={700} onSetActive={handleSetActive}>
         Compétances
-      </a>
-      <a className="navPage" href="#xp">
-        Expériences
-      </a>
-      <a className="navPage" href="#projects">
-        Mes réalisations
-      </a>
-      <a className="navPage" href="#description">
+      </Link>
+      <Link className="navPage" activeClass="active" to="xp" spy={true} smooth={true} offset={0} duration={700} onSetActive={handleSetActive}>
+          Expériences
+      </Link>
+      <Link className="navPage" activeClass="active" to="projects" spy={true} smooth={true} offset={0} duration={700} onSetActive={handleSetActive}>
+         Mes réalisations
+      </Link>
+      <Link className="navPage" activeClass="active" to="description" spy={true} smooth={true} offset={0} duration={700} onSetActive={handleSetActive}>
         Qui suis-je ?
-      </a>
-      <a className="navPage" href="#contact">
-        Contact
-      </a>
+      </Link>
+      <Link className="navPage" activeClass="active" to="contact" spy={true} smooth={true} offset={0} duration={700} onSetActive={handleSetActive}>
+         Contact
+      </Link>
+    </div>
     </div>
   );
 };
