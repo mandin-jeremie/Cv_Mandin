12e02c66a3ce53f2f24a9e27291c0c630212facf
Ubuntu-WSL <mandin.j@gmail.com>
2020-08-21 15:25:25 +0200

    Xp page with a time line components of materialUI

--- src/container/Xp/index.js
+++ src/container/Xp/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import TimeLine from "../../components/TimeLine/index.js"
 import "./xp.css";
 
 const Xp = () => {
   return (
     <div className="bloc_xp" id="xp">
-      <h3>Expériences</h3>
+      <h3 className="subTitle_xp">Expériences Professionnelles</h3>
+      <div className="Bloc_TimeLine">
+      <TimeLine />
+      </div>
     </div>
   );
 };
